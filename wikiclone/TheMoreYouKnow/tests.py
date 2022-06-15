efe5d65d9d21539b4a0cb4d9d8ed49637c4b1fd9
rettichsimonlel <retter190085@sr.htlweiz.at>
2022-06-15 08:14:00 +0200

    login with test

--- wikiclone/TheMoreYouKnow/tests.py
+++ wikiclone/TheMoreYouKnow/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.test.client import RequestFactory
 
 # Create your tests here.
+class LoginTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        new_user = User(username="Simon")
+        new_user.set_password("1234")
+        new_user.save()
+    
+    def test_login(self):
+        request = self.factory.get('/customer/details')
+        yes = True
+        try:
+            user = authenticate(request, username="Simon", password="1234")
+        except:
+            yes = False
+        self.assertEqual(yes, True)
